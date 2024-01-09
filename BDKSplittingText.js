04321e00082eade13fb7a5efd38a008bd1e1f553
M Hassan Khan <111183437+BDK57@users.noreply.github.com>
2024-01-09 20:05:44 +0500

    Update BDKSplittingText.js

--- BDKSplittingText.js
+++ BDKSplittingText.js
@@ -1,38 +1,56 @@
-window.onload = function () {
-          
-    // let elements = document.querySelectorAll(".bdkSplitting");
-    let elements = document.querySelectorAll('[bdkSplitting]')
+   window.onload = function () {
+        // let totalWords = 0;
+        // let totalChars = 0;
+        // let elements = document.querySelectorAll(".bdkSplitting");
+        let elements = document.querySelectorAll("[bdkSplitting]");
 
-  elements.forEach(function (e) {
-    let text = e.textContent;
-    e.textContent = "";
-  e.classList.add('BDK-Splitting')
-    let words = text.split(" ");
+     elements.forEach(function (e) {
+          let text = e.textContent;
+          e.textContent = "";
+          e.classList.add("BDK-Splitting");
+          let words = text.split(" ");
+          // let totalChars = 0;
+          // console.log(totalChars)
+          // totalChars = 0
+          let charsT =0;
+          words.forEach(function (word, index) {
+            if (word !== "") {
+              let wordSpan = document.createElement("span");
+            wordSpan.className = "words";
+            wordSpan.setAttribute("data-word", word);
+            // totalWords = totalWords + index;
 
-    words.forEach(function (word) {
-      let wordSpan = document.createElement("span");
-      wordSpan.className = "words";
-      wordSpan.setAttribute("data-word", word);
+            // wordSpan.innerText = word;
+       
+            let whitespaceSpan = document.createElement("span");
+            whitespaceSpan.className = "whitespace";
+            whitespaceSpan.innerText = " ";
 
-      // wordSpan.innerText = word;
+            e.appendChild(wordSpan);
+            e.appendChild(whitespaceSpan);
 
-      let whitespaceSpan = document.createElement("span");
-      whitespaceSpan.className = "whitespace";
-      whitespaceSpan.innerText = " ";
+            let chars = word.split("");
+        
+            charsT += chars.length
 
-      e.appendChild(wordSpan);
-      e.appendChild(whitespaceSpan);
+       
 
-      let chars = word.split("");
+            chars.forEach(function (char, index) {
+              let charSpan = document.createElement("span");
+              charSpan.className = "char";
+              charSpan.innerText = char;
+              charSpan.setAttribute("data-char", char);
+              // totalChars =totalChars+ chars.length;
+              wordSpan.appendChild(charSpan);
+            });
 
-      chars.forEach(function (char) {
-        let charSpan = document.createElement("span");
-        charSpan.className = "char";
-        charSpan.innerText = char;
-        charSpan.setAttribute("data-char", char);
-
-        wordSpan.appendChild(charSpan);
-      });
-    });
-  });
-};
+            }
+           
+            // totalWords = 0;
+            
+            
+          });
+          e.style = `--word-total: ${words.length}; --char-total:  ${charsT}; font-size: 46px; color: cyan`;
+      
+        });
+      };
